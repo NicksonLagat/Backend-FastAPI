31984b5d92c69fb88f0c66bcef510e9e66b3e031
NicksonLagat <73611868+NicksonLagat@users.noreply.github.com>
2021-02-15 00:48:33 +0300

    path and query parameter

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello")
+from typing import Optional
+from fastapi import FastAPI
+
+# instantiate fastapi app
+
+app = FastAPI()
+
+@app.get('/')
+async def hello_world():
+    return {'Hello':'World'}
+
+@app.get("/component/{component_id}") #path parameter
+async def get_component(component_id:int):
+    return {"component_id":"component_id"}
+
+@app.get("/component/") 
+async def read_component(number:int,text:str):#query parameter
+    return {"number":number,"text":text}
+
+
+    
+
+
+
